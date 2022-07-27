f0a22d0f48d9cad528804bb36cf290ec74f00c5c
yang vieira <yangflix3@gmail.com>
2022-07-26 22:58:15 -0400

    :art: chega

--- src/month-name/MonthNameInCalendar.cs
+++ src/month-name/MonthNameInCalendar.cs
@@ -6,18 +6,18 @@ public class MonthNameInCalendar
 
     public static string MonthName(int month)
     {
-        if(month == 1) return "janeiro";
-        if(month == 2) return "fevereiro";
-        if(month == 3) return "março";
-        if(month == 4) return "abril";
-        if(month == 5) return "maio";
-        if(month == 6) return "junho";
-        if(month == 7) return "julho";
-        if(month == 8) return "agosto";
-        if(month == 9) return "setembro";
-        if(month == 10) return "outubro";
-        if(month == 11) return "novembro";
-        if(month == 12) return "dezembro";
+        if(month == 1) return "Janeiro";
+        if(month == 2) return "Fevereiro";
+        if(month == 3) return "Março";
+        if(month == 4) return "Abril";
+        if(month == 5) return "Maio";
+        if(month == 6) return "Junho";
+        if(month == 7) return "Julho";
+        if(month == 8) return "Agosto";
+        if(month == 9) return "Setembro";
+        if(month == 10) return "Outubro";
+        if(month == 11) return "Novembro";
+        if(month == 12) return "Dezembro";
         return "inválido";
     }
 }
