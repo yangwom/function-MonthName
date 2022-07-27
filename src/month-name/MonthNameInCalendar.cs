86052324ad1921e30dabbc9fb65f7f83814ba3a2
yang vieira <yangflix3@gmail.com>
2022-07-26 22:34:04 -0400

    :sparkles: feat function MonthName

--- src/month-name/MonthNameInCalendar.cs
+++ src/month-name/MonthNameInCalendar.cs
@@ -1,13 +1,23 @@
 ﻿namespace month_name;
 public class MonthNameInCalendar
 {
-    /// <summary>
-    /// Esta função retorna o nome do mês passado como parâmetro de entrada.
-    /// </summary>
-    /// <param month="mes"> Um número inteiro com o mês. </param>   
-    /// <returns> Nome do mês</returns>
+  
+  
+
     public static string MonthName(int month)
     {
-        throw new NotImplementedException();
+        if(month == 1) return "Janeiro";
+        if(month == 2) return "Fevereiro";
+        if(month == 3) return "Março";
+        if(month == 4) return "Abril";
+        if(month == 5) return "Maio";
+        if(month == 6) return "Junho";
+        if(month == 7) return "julho";
+        if(month == 8) return "Agosto";
+        if(month == 9) return "Setembro";
+        if(month == 10) return "Outubro";
+        if(month == 11) return "Novembro";
+        if(month == 12) return "Dezembro";
+        return "esse mes não existe";
     }
 }
